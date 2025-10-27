80a5564bd8dd366e83bf6f69dde683985fec4095
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-10-27 13:25:53 +0100

    feat(food-entry): use luxon for date formatting in queries

--- apps/server/src/food-entry/food-entry.repository.ts
+++ apps/server/src/food-entry/food-entry.repository.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { eq } from 'drizzle-orm';
+import { DateTime } from 'luxon';
 import {
   DRIZZLE_PROVIDER,
   type DrizzleDatabase,
@@ -10,7 +11,7 @@ import { FoodEntry, foodEntries } from 'src/database/schema/other.schema';
 export class FoodEntryRepository {
   constructor(@Inject(DRIZZLE_PROVIDER) private readonly db: DrizzleDatabase) {}
   async getEntries(date: Date): Promise<FoodEntry[]> {
-    const dateString = date.toISOString().slice(0, 10);
+    const dateString = DateTime.fromJSDate(date).toFormat('yyyy-M-dd');
     return await this.db.query.foodEntries.findMany({
       where: eq(foodEntries.entryDate, dateString),
     });
