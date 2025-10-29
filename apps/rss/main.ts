7491e4872157e99fad121d4081799efc4ee553d0
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-10-29 12:05:29 +0100

    feat(rss): add new RSS feed app with example feed generation

---
+++ apps/rss/main.ts
@@ -0,0 +1,29 @@
+import { Rss } from "@feed/feed";
+
+const rssFeed = new Rss({
+  title: "RSS Feed Example",
+  description: "A simple RSS feed example",
+  link: "http://example.com/rss-feed",
+  updated: new Date("2024-10-19T15:12:56Z"),
+  id: "http://example.com/rss-feed",
+  authors: [
+    {
+      name: "John Doe",
+      email: "test@example.org",
+    },
+  ],
+});
+
+rssFeed.addItem({
+  title: "First RSS Item",
+  link: "http://example.com/rss1",
+  id: "http://example.com/rss1",
+  updated: new Date("2024-10-19T15:12:56Z"),
+  description: "Description for RSS item 1",
+  content: {
+    body: "Content for RSS item 1",
+    type: "html",
+  },
+});
+
+Deno.writeTextFileSync("example.xml", rssFeed.build());
