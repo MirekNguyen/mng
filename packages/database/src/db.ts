9780743fe3e8ee99a718b2b78db6e71eb086d984
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-12-07 22:31:27 +0100

    refactor(config): centralize TS config in shared package

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { env } from "./env";
-import { properties } from "./schema/properties.schema";
 import { otherSchema } from "./schema/other.schema";
+import { properties } from "./schema/properties.schema";
+import { env } from "./env";
 
 const schema = {
   properties,
