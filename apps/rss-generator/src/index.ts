a59a98089172d7323f0717b0f8c912c333686551
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-12-07 17:43:53 +0100

    feat(rss-generator): add new YouTube RSS feed generator app

---
+++ apps/rss-generator/src/index.ts
@@ -0,0 +1,73 @@
+import { program } from "commander";
+import YouTube from "youtube-sr";
+import { Feed } from "feed";
+import { write } from "bun";
+import pino from "pino";
+
+const logger = pino({
+  level: "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateTime: "yyyy-mm-dd HH:MM:ss",
+      ignore: "pid,hostname",
+    },
+  },
+});
+
+program
+  .requiredOption("-p, --playlistId <string>", "Query using Playlist ID")
+  .requiredOption("-o, --output <string>", "Output file path");
+
+program.parse(process.argv);
+const options = program.opts();
+
+const getPlaylist = async (playlistId: string) => {
+  try {
+    return await YouTube.getPlaylist(playlistId, {
+      limit: 3,
+    });
+  } catch (error) {
+    logger.error("Error fetching channel videos.");
+    if (error instanceof Error) logger.error(`Error message: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const playlist = await getPlaylist(options.playlistId);
+const videos = playlist.videos;
+
+const feed = new Feed({
+  title: options.channel
+    ? `Channel Uploads: ${options.query}`
+    : "Youtube Search Feed",
+  description: "Youtube subscriptions feed",
+  id: `https://mirekng.com/rss/${options.output}`,
+  link: "https://mirekng.com",
+  language: "en",
+  updated: new Date(),
+  generator: "Bun RSS Generator",
+  copyright: "",
+});
+
+videos.forEach((video) => {
+  feed.addItem({
+    title: `${video.title}`,
+    id: video.url,
+    link: video.url,
+    description: `
+        <img src="${video.thumbnail?.url || ""}" /><br>
+        ${video.url}<br>
+        Published: ${video.uploadedAt}<br>
+        Duration: ${video.durationFormatted}
+      `,
+    date: new Date(video.uploadedAt ?? ""),
+  });
+});
+
+await write(`out/${options.output}`, feed.rss2());
+
+logger.info(`Successfully generated RSS feed at: ${options.output}`);
+logger.info(`Total videos found: ${videos.length}`);
+
