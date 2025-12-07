254751144a17cc1e58010eb1fe3a21c871840a23
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-12-07 22:25:14 +0100

    feat(elysia): add food-entry route with date query validation

--- apps/elysia/src/index.ts
+++ apps/elysia/src/index.ts
@@ -1,5 +1,7 @@
-import { db } from "@mng/database/db";
+import { db, eq } from "@mng/database/db";
+import { foodEntries } from "@mng/database/schema/other.schema";
 import Elysia from "elysia";
+import z from "zod";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
@@ -10,6 +12,19 @@ app.get("properties", async () => {
 app.get("food", async () => {
   return await db.query.food.findMany();
 });
+
+app.get(
+  "food-entry",
+  async ({ query }) => {
+    const dateString = query.date.toISOString().split("T")[0];
+    return await db.query.foodEntries.findMany({
+      where: eq(foodEntries.entryDate, dateString),
+    });
+  },
+  {
+    query: z.object({ date: z.coerce.date() }),
+  },
+);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
