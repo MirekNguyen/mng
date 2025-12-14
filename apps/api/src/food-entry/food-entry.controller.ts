4dbb4504629988c88f456e12cde35f046c2f6327
Mirek Nguyen <mirek.nguyen@o2.cz>
2025-12-14 20:42:09 +0100

    feat(food-entry): add update and delete endpoints

--- apps/api/src/food-entry/food-entry.controller.ts
+++ apps/api/src/food-entry/food-entry.controller.ts
@@ -3,9 +3,11 @@ import {
   createFoodEntrySchema,
   foodEntries,
   FoodEntry,
+  updateFoodEntrySchema,
 } from "@mng/database/schema/other.schema";
 import Elysia from "elysia";
 import z from "zod";
+import { FoodEntryRepository } from "./food-entry.repository";
 
 const app = new Elysia({ prefix: "food-entry" });
 
@@ -29,6 +31,27 @@ app.post(
   },
   {
     body: createFoodEntrySchema,
+  },
+);
+
+app.patch(
+  "/:id",
+  async ({ query, params }): Promise<FoodEntry> => {
+    return FoodEntryRepository.update(params.id, query);
+  },
+  {
+    query: updateFoodEntrySchema,
+    params: z.object({ id: z.number().positive() }),
+  },
+);
+
+app.delete(
+  "/:id",
+  async ({ params }): Promise<FoodEntry> => {
+    return FoodEntryRepository.delete(params.id);
+  },
+  {
+    params: z.object({ id: z.number().positive() }),
   },
 );
 
