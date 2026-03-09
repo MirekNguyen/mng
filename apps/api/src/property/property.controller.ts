609b04d5750c6da16ccbdcce91322352949b2172
Mirek Nguyen <mirek.nguyen@o2.cz>
2026-03-09 13:14:24 +0100

    feat(property): add POST /scrape endpoint

--- apps/api/src/property/property.controller.ts
+++ apps/api/src/property/property.controller.ts
@@ -1,11 +1,28 @@
 import { db } from "@mng/database/db";
 import { Property } from "@mng/database/schema/properties.schema";
+import { scrapeEstatesFromUrls } from "@mng/scraper/sreality.scraper";
 import Elysia from "elysia";
+import z from "zod";
+
+import { PropertyRepository } from "./property.repository";
 
 const app = new Elysia({ prefix: "property" });
 
 app.get("/", async (): Promise<Property[]> => {
   return await db.query.properties.findMany();
 });
+
+app.post(
+  "/scrape",
+  async ({ body }): Promise<Property[]> => {
+    const scraped = await scrapeEstatesFromUrls(body.urls);
+    return PropertyRepository.upsertMany(scraped);
+  },
+  {
+    body: z.object({
+      urls: z.array(z.string().url()).min(1),
+    }),
+  },
+);
 
 export { app as propertyController };
