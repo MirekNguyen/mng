77131e21e252cf59eda9524b9dbc98a930404420
Mirek Nguyen <mirek.nguyen@o2.cz>
2026-05-14 22:46:35 +0200

    refactor(database): validate env variables via zod

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -1,7 +1,7 @@
 import { otherSchema } from "./schema/other.schema";
 import { properties } from "./schema/properties.schema";
 import { rssSchema } from "./schema/rss.schema";
-import { env } from "./env";
+import { dbSchema } from "./env.zodschema";
 import { drizzle } from "drizzle-orm/bun-sql";
 
 const schema = {
@@ -10,6 +10,7 @@ const schema = {
   ...rssSchema,
 };
 
-export const db = drizzle(env.DATABASE_URL, { schema });
+const dbEnv = dbSchema.parse(process.env);
+export const db = drizzle(dbEnv.DATABASE_URL, { schema });
 
 export * from "drizzle-orm";
